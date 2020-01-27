5bd87428d152764b350c965bc33bd5af5b63b12b
nikhil.karkra@rmit.edu.au <nikhil.karkra@rmit.edu.au>
2020-01-28 00:16:49 +1100

    completed pet schema

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,51 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
-
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
+import { ApolloLink } from "apollo-link";
+import { setContext } from "apollo-link-context";
 /**
  * Create a new apollo client and export as default
  */
+//"https://rickandmortyapi.com/graphql"
+const typeDefs = gql`
+  extend type User {
+    age: Int
+  }
+  extend type Pet{
+      vaccinated:Boolean!
+  }
+`;
+const resolvers = {
+  User: {
+    age() {
+      return 35;
+    }
+  },
+  Pet: {
+    vaccinated(){
+        return true
+    }
+  }
+};
+const http = new HttpLink({
+  uri: "http://localhost:5000/"
+});
+
+const delay = setContext(
+  request =>
+    new Promise((success, fail) => {
+      setTimeout(() => {
+        success();
+      }, 800);
+    })
+);
+const link = ApolloLink.from([delay, http]);
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link,
+  cache,
+  resolvers,
+  typeDefs
+});
+export default client;
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,21 +1,82 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import PetsList from "../components/PetsList";
+import NewPetModal from "../components/NewPetModal";
+import Loader from "../components/Loader";
 
+// client side directives aree
+// @skip() - to skip the fields
+// @include() - to include
 
-export default function Pets () {
-  const [modal, setModal] = useState(false)
+const PETS_FIELDS = gql`
+  fragment PetsFields on Pet {
+    id
+    name
+    type
+    img
+    vaccinated @client
+    owner {
+      id
+      age @client
+    }
+  }
+`;
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`;
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`;
 
+export default function Pets() {
+  const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(ALL_PETS);
+  const [createPet, newPet] = useMutation(NEW_PET, {
+    update(cache, { data: { addPet } }) {
+      const data = cache.readQuery({ query: ALL_PETS }); // fetch query
+      cache.writeQuery({
+        query: ALL_PETS, // query to update
+        data: { pets: [addPet, ...data.pets] }
+      });
+    }
+  });
 
   const onSubmit = input => {
-    setModal(false)
+    setModal(false);
+    createPet({
+      variables: { newPet: input },
+      optimisticResponse: {
+        __typename: "Mutation",
+        addPet: {
+          __typename: "Pet", // return type of mutation
+          id: Math.floor(Math.random() * 1000) + "",
+          name: input.name,
+          type: input.type,
+          img: "https://via.placeholder.com/300"
+        }
+      }
+    });
+  };
+  if (loading) {
+    return <Loader />;
   }
-  
+  if (error || newPet.error) {
+    return <p>Error occured</p>;
+  }
+  console.log(data.pets[0]);
   if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
   }
 
   return (
@@ -32,8 +93,8 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
-  )
+  );
 }
